db3b67203bb7e672d295ed4306c7140bf5a5a898
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-06-29 14:42:46 +0200

    registermobil page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
 
 function Home() {
   const [{ user }] = useUserContext();
   return (
     <>
-      {user ? <h1>Bonjour {user.firstname}</h1> : null}
+      {user ? (
+        <h1 className="HelloSandrine">Bonjour {user.firstname}</h1>
+      ) : null}
       <div className="titre">
         <h1>Commencer l'enregistrement de nouveaux téléphones</h1>
       </div>
       <div className="button-container">
-        <button type="button" className="blue-button">
-          <p>Identification</p>
-        </button>
+        <Link to="/register">
+          <button type="button" className="blue-button">
+            <p>Identification</p>
+          </button>
+        </Link>
       </div>
       <img
         src="src/assets/Images/image-home.png"
